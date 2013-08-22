376c4ee0c479bba58b84e7b8e0e0c6b4f1cb385d
Chiel Kas <chielkas@gmail.com>
2013-08-22 12:49:40 +0200

    Changed metric voor lan gateway, should solve dns problems

--- route1.bat
+++ route1.bat
@@ -1,3 +1,3 @@
 route change 0.0.0.0 mask 0.0.0.0 10.36.128.1 metric 5
-route add 10.0.0.0 mask 255.0.0.0 10.56.43.1
-route add 10.36.0.0 mask 255.255.0.0 10.36.128.1
+route add 10.0.0.0 mask 255.0.0.0 10.56.43.1 metric 200
+route add 10.36.0.0 mask 255.255.0.0 10.36.128.1 metric 5
